05a1d0f2815432b3b57f606f8a12c1ca29d19474
Reid Miller <reid.s.miller@gmail.com>
2023-02-17 12:20:07 -0700

    test: Add board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,15 +7,24 @@ RSpec.describe Board do
 
   describe '#Initialize' do
     it 'exists' do
-      expect(board).to be_instance_of(Board)
+      expect(@board).to be_instance_of(Board)
     end
 
-    it 'can contain an array of cells' do
-
+    it 'creates an array of 6 elements that are arrays of 7' do
+      expect(@board.board_array.length).to eq(6)
+      expect(@board.board_array[1].length).to eq(7)
     end
 
-    it 'can convert cells to terminal output' do
-      
+    xit 'can print the board' do
+      expect{@board.render}.to output(
+        " A B C D E F G
+ . . . . . . .
+ . . . . . . .
+ . . . . . . .
+ . . . . . . .
+ . . . . . . .
+ . . . . . . ."
+      ).to_stdout
     end
   end
 end
