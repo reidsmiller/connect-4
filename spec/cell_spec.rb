d1acf68c587471406a54df52acb6ba4b6ea2a3af
Reid Miller <reid.s.miller@gmail.com>
2023-02-16 17:32:41 -0700

    test: Update cell tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,27 +3,29 @@ require 'spec_helper'
 RSpec.describe Cell do
   describe '#initialize' do
     it 'exists' do
-      a1 = Cell.new
+      a1 = Cell.new('a1')
 
       expect(a1).to be_instance_of(Cell)
     end
 
     it 'has a default value of "."' do
-      a1 = Cell.new
+      a1 = Cell.new(:a1)
 
-      expect(a1.display_chr). to eq(".")
+      expect(a1.value). to eq(".")
     end
 
-    it 'can change value if selected by player' do
-      a1 = Cell.new
+    xit 'can change value if selected by player' do
+      a1 = Cell.new(:a1)
 
-      expect(a1.player). to eq("X")
+      a1.player_select
+      expect(a1.value). to eq("X")
     end
 
-    it 'can change value if selected by computer' do
-      a1 = Cell.new
+    xit 'can change value if selected by computer' do
+      a1 = Cell.new(:a1)
 
-      expect(a1.comp).to eq("O")
+      a1.comp_select
+      expect(a1.value).to eq("O")
     end
   end
 end
