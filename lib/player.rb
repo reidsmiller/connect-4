53ddf5eb7979a60c26e827822d8a81550c279888
Reid Miller <reid.s.miller@gmail.com>
2023-02-17 15:27:08 -0700

    feat: Add covert method to player

--- lib/player.rb
+++ lib/player.rb
@@ -4,4 +4,20 @@ class Player
   def initialize(name)
     @name = name
   end
+
+  def place(input)
+    input.upcase!
+  end
+
+  def convert(letter)
+    case letter
+      when "A" then 0
+      when "B" then 1
+      when "C" then 2
+      when "D" then 3
+      when "E" then 4
+      when "F" then 5
+      when "G" then 6
+    end
+  end
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -5,3 +5,5 @@ board = Board.new
 puts 'Welcome to connect 4!!'
 
 board.render
+
+player1.place(gets.chomp)
