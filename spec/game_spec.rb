fb1b3dcdff540b26825f2dac014acfb2d8faac25
Thomas Hawley <thawley22@turing.edu>
2023-02-18 10:53:42 -0700

    fix: Rename win method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -31,10 +31,10 @@ RSpec.describe Game do
     @game.player_turn("B")
     @game.player_turn("C")
     
-    expect(@game.won?).to be false
+    expect(@game.win?).to be false
 
     @game.player_turn("D")
 
-    expect(@game.won?).to be true
+    expect(@game.win?).to be true
   end
 end
