77c00c8e00dd83d12259abb22118bce0996bc0d0
Thomas Hawley <thawley22@turing.edu>
2023-02-18 11:44:52 -0700

    test: Add test for computer win horizontal, add test for vertical win

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -37,4 +37,45 @@ RSpec.describe Game do
     
     expect(@game.game_win).to be true
   end
+
+  it 'returns false if the computer has 4 pieces in a row horizontally' do
+    #forcing "O"'s since our computer is not that smart yet
+
+    @game.board.place("A", "O")
+    @game.board.place("B", "O")
+    @game.board.place("C", "O")
+
+    expect(@game.game_win).to be nil
+
+    @game.board.place("D", "O")
+    @game.win?
+
+    expect(@game.game_win).to be false
+  end
+
+  it 'returns true when 4 pieces are in a row virtically' do
+    @game.player_turn("B")
+    @game.player_turn("B")
+    @game.player_turn("B")
+
+    expect(@game.game_win).to be nil
+
+    @game.player_turn("B")
+
+    expect(@game.game_win).to be true
+  end
+
+  it 'returns false when 4 computer pieces are in a row virtically' do
+    #forcing "O"'s since our computer is not that smart yet
+    @game.board.place("C", "O")
+    @game.board.place("C", "O")
+    @game.board.place("C", "O")
+
+    expect(@game.game_win).to be nil
+
+    @game.board.place("C", "O")
+    @game.win?
+
+    expect(@game.game_win).to be false
+  end
 end
