2e8b5325ebd7ae8d43c24a34bd20dbf6eb16dbaf
Thomas Hawley <thawley22@turing.edu>
2023-02-18 11:23:12 -0700

    feat: Add #check_horizontal win

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,21 @@
 class Game
 
-  attr_reader :board
+  attr_reader :board, :game_win
   def initialize
     @board = Board.new
     @turns = []
+    @game_win = nil
   end
 
   def player_turn(column)
     @board.place(column, "X")
+    self.win?
   end
 
   def computer_turn
     comp_column = (65 + rand(7)).chr
     @board.place(comp_column, "O")
+    self.win?
     comp_column
   end
 
@@ -21,10 +24,15 @@ class Game
   end
 
   def check_horizontal
+    horizontal = []
     @board.board_array.each do |row|
-      row[0..4].each_cons(4)
-      require 'pry'; binding.pry
+      row.each_cons(4) do |location|
+        if location == ["X", "X", "X", "X"]
+          @game_win = true
+        elsif location == ["O", "O", "O", "O"] 
+          @game_win = false
+        end
+      end
     end
-    
   end
 end
