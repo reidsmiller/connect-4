8feb728c42be6018f51e16738107118c989602fb
Thomas Hawley <thawley22@turing.edu>
2023-02-17 20:36:43 -0700

    test: Remove tests and moved to board tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,24 +15,4 @@ RSpec.describe Player do
       expect(@player1.name).to eq("Rick")
     end
   end
-
-  describe '#Pieces' do
-    it 'can call particular column' do
-      expect(@player1.convert("B")).to eq(1)
-      expect(@player1.convert("F")).to eq(5)
-    end
-
-    it 'can place a piece at appropriate place in column' do
-      @player1.place("B")
-      @player1.place("D")
-      @player1.place("B")
-
-      expect(@player1.board.board_array).to eq([[".", ".", ".", ".", ".", ".", "."],
-        [".", ".", ".", ".", ".", ".", "."],
-        [".", ".", ".", ".", ".", ".", "."],
-        [".", ".", ".", ".", ".", ".", "."],
-        [".", "X", ".", ".", ".", ".", "."],
-        [".", "X", ".", "X", ".", ".", "."]])
-    end
-  end
 end
