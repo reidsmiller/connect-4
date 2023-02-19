16a2db1787aaf094f4c3c2787c5a7e8c20df1b25
Reid Miller <reid.s.miller@gmail.com>
2023-02-19 10:12:17 -0700

    feat: Update runner.rb

--- lib/runner.rb
+++ lib/runner.rb
@@ -2,8 +2,7 @@ require_relative 'board'
 
 board = Board.new
 
-puts 'Welcome to connect 4!!'
+puts 'Welcome to Connect 4!!/n
+Enter p to play the highest stakes game of your life. Enter q to wimp out and quit.'
 
 board.render
-
-player1.place(gets.chomp)
