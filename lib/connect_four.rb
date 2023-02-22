dd6f9d7f9cba1e9693b98377728de126d0bf50fd
Reid Miller <reid.s.miller@gmail.com>
2023-02-22 16:26:31 -0700

    feat: Add executible file

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "connect_four/version"
-# require './lib/board'
-# require './lib/game'
-# require './lib/game_play'
-# require './lib/playable'
+require './lib/connect_four/board'
+require './lib/connect_four/game'
+require './lib/connect_four/game_play'
+require './lib/connect_four/playable'
 
 
 
