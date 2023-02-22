29a46c1b32b2a5b09066c7aa4fa8bad1c7fd28c3
Reid Miller <reid.s.miller@gmail.com>
2023-02-22 15:56:40 -0700

    fix: Move files into bundler

---
+++ lib/connect_four.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "connect_four/version"
+# require './lib/board'
+# require './lib/game'
+# require './lib/game_play'
+# require './lib/playable'
+
+
+
+module ConnectFour
+  class Error < StandardError; end
+  # Your code goes here...
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
-require 'rspec'
-require './lib/board'
-require './lib/game'
-require './lib/game_play'
-require './lib/playable'
+# frozen_string_literal: true
+
+require "connect_four"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
