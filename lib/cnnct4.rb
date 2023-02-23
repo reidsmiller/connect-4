4783e459117eceb59cfde34169ac5f85463dd970
Reid Miller <reid.s.miller@gmail.com>
2023-02-22 20:53:14 -0700

    test: Add test for cell class

--- lib/cnnct4.rb
+++ lib/cnnct4.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "cnnct4/version"
+require_relative 'cnnct4/cell'
 require_relative 'cnnct4/board'
 require_relative 'cnnct4/game'
 require_relative 'cnnct4/game_play'
